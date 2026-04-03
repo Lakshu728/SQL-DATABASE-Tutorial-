1b65bcb7ce0977dc06858c43151908cd5464668f
Lakshita728 <lakshitarathor05@gmail.com>
2026-04-03 16:46:18 +0530

    Added SQL and notes files

---
+++ 01_Basics/create_database_basic.sql
@@ -0,0 +1,11 @@
+create database startsql;
+USE startsql;
+CREATE table users(
+id int auto_increment primary key,
+name varchar(100) not null,
+email varchar(100) unique not null,
+gendar enum('Male', 'Female', 'Other'),
+date_of_birth date,
+created_at timestamp default current_timestamp
+);
+select * from startsql,
